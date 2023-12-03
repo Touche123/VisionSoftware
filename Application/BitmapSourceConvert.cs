eabc0a8b8c2ef37c2dcd87d92e561f13c5b883f5
Jonas Westman <jonaswestman@hotmail.com>
2023-12-03 14:31:10 +0100

    second commit..

---
+++ Application/BitmapSourceConvert.cs
@@ -0,0 +1,50 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MainApp
+{
+    internal class BitmapSourceConvert : IValueConverter
+    {
+        /// <summary>
+        /// mat to bitmapsource
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Mat mat)
+            {
+                return mat.ToBitmapSource();
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// bitmapsource to mat
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is BitmapSource bitmap)
+            {
+                return bitmap.ToMat();
+            }
+            return null;
+        }
+    }
+}
---
+++ Application/MainWindowViewModel.cs
@@ -0,0 +1,335 @@
+﻿using Microsoft.Win32;
+using OpenCvSharp;
+using OpenCvSharp.Flann;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MainApp
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        public MainWindowViewModel()
+        {
+            LoadCommand = new DelegateCommand<string>(LoadExecute);
+            TrainCommand = new DelegateCommand(TrainTemplate);
+            SearchCommand = new DelegateCommand(MatchSearch);
+            AllocConsole();
+
+            DNNWindowViewModel dNNWindowViewModel = new DNNWindowViewModel();
+            dNNWindowViewModel.Run();
+        }
+
+        public ICommand LoadCommand { get; private set; }
+        public ICommand TrainCommand { get; private set; }
+        public ICommand SearchCommand { get; private set; }
+
+        private Mat template;
+        private Mat destination;
+
+        /// <summary>
+        /// Image used to create template
+        /// </summary>
+        public Mat Template
+        {
+            get { return template; }
+            set
+            {
+                template = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Image to be processed
+        /// </summary>
+        public Mat Destination
+        {
+            get { return destination; }
+            set
+            {
+                destination = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private List<PointInfo> results = new List<PointInfo>();
+
+        /// <summary>
+        /// Load template and destination image
+        /// </summary>
+        /// <param name="i"></param>
+        private void LoadExecute(string i)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Path.Combine(Path.GetDirectoryName(GetType().Assembly.Location), "assets");
+            dialog.Filter = "All|*.*|jpg|*.jpg|png|*.png|bmp|*.bmp";
+            if (dialog.ShowDialog().Value == true)
+            {
+                if (dialog.CheckFileExists)
+                {
+                    string file = dialog.FileName;
+                    if (i.ToLower().Contains("t"))
+                    {
+                        Template?.Dispose();
+                        Template = new Mat(file);
+                    }
+                    else
+                    {
+                        Destination?.Dispose();
+                        Destination = new Mat(file);
+                    }
+                }
+            }
+        }
+
+        private void TrainTemplate()
+        {
+            try
+            {
+                using (Mat src = new Mat())
+                using (Mat output = new Mat())
+                using (Mat gx = new Mat())
+                using (Mat gy = new Mat())
+                using (Mat magnitude = new Mat())
+                using (Mat direction = new Mat())
+                {
+
+
+                    results.Clear();
+
+                    Cv2.CvtColor(template, src, ColorConversionCodes.RGB2GRAY);
+                    Cv2.PyrDown(src, src);
+                    Cv2.PyrDown(src, src);
+                    Cv2.PyrDown(src, src);
+
+                    Cv2.Canny(src, output, 100, 100 * 3);
+                    Cv2.FindContours(output, out var contours, out var hierarchy, RetrievalModes.List, ContourApproximationModes.ApproxNone);
+
+                    Cv2.Sobel(src, gx, MatType.CV_64F, 1, 0, 3);
+                    Cv2.Sobel(src, gy, MatType.CV_64F, 0, 1, 3);
+
+                    Cv2.CartToPolar(gx, gy, magnitude, direction);
+
+                    var sum = new Point2d(0, 0);
+                    for (int i = 0, m = contours.Length; i < m; i++)
+                    {
+                        for (int j = 0, n = contours[i].Length; j < n; j++)
+                        {
+                            var cur = contours[i][j];
+                            var fdx = gx.At<double>(cur.Y, cur.X, 0); // dx
+                            var fdy = gy.At<double>(cur.Y, cur.X, 0); // dy
+                            var der = new Point2d(fdx, fdy); // (dx,dy)
+                            var mag = magnitude.At<double>(cur.Y, cur.X, 0); // √(dx²+dy²)
+                            var dir = direction.At<double>(cur.Y, cur.X, 0); // atan2(dy,dx)
+                            results.Add(new PointInfo
+                            {
+                                Point = cur,
+                                Derivative = der,
+                                Direction = dir,
+                                Magnitude = mag == 0 ? 0 : 1 / mag,
+                            });
+                            sum += cur;
+                        }
+                    }
+
+                    /// update Center and Offset in PointInfo
+                    var center = new Point2d(sum.X / results.Count, sum.Y / results.Count);
+                    foreach (var item in results)
+                    {
+                        item.Update(center);
+                    }
+
+                    /// overlay display origin image, edge(green) and center point(red)
+                    Cv2.DrawContours(template, new[] { results.Select(_ => _.Point) }, -1, Scalar.LightGreen, 2);
+                    //Cv2.DrawContours(template, contours, -1, Scalar.LightGreen, 2);
+                    Cv2.Circle(template, center.ToPoint(), 2, Scalar.Red, -1);
+
+                    /// update UI
+                    RaisePropertyChanged(nameof(Template));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                Trace.TraceError(ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// NCC to find template
+        /// </summary>
+        private void MatchSearch()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            try
+            {
+                Trace.TraceInformation("NCC matching start");
+                stopwatch.Start();
+
+                /// convert to gray image
+                using (Mat src = new Mat())
+                using (Mat gx = new Mat())
+                using (Mat gy = new Mat())
+                using (Mat direction = new Mat())
+                using (Mat magnitude = new Mat())
+                {
+
+                    Cv2.CvtColor(destination, src, ColorConversionCodes.RGB2GRAY);
+                    Cv2.PyrDown(src, src);
+                    Cv2.PyrDown(src, src);
+                    Cv2.PyrDown(src, src);
+                    /// use the sobel filter on the source image which returns the gradients in the X (Gx) and Y (Gy) direction.
+                    Cv2.Sobel(src, gx, MatType.CV_64F, 1, 0, 3);
+                    Cv2.Sobel(src, gy, MatType.CV_64F, 0, 1, 3);
+
+                    /// compute the magnitude and direction
+                    Cv2.CartToPolar(gx, gy, magnitude, direction);
+
+                    var minScore = 0.7;
+                    var greediness = 0.8;
+
+                    /// ncc match search
+                    long noOfCordinates = results.Count;
+                    double normMinScore = minScore / noOfCordinates; // normalized min score
+                    double normGreediness = (1 - greediness * minScore) / (1 - greediness) / noOfCordinates;
+                    double partialScore = 0;
+                    double resultScore = 0;
+                    Point center = new Point();
+
+                    for (int i = 0, h = src.Height; i < h; i++)
+                    {
+                        for (int j = 0, w = src.Width; j < w; j++)
+                        {
+                            double partialSum = 0;
+                            for (var m = 0; m < noOfCordinates; m++)
+                            {
+                                var item = results[m];
+                                var curX = (int)(j + item.Offset.X);
+                                var curY = (int)(i + item.Offset.Y);
+                                var iTx = item.Derivative.X;
+                                var iTy = item.Derivative.Y;
+                                if (curX < 0 || curY < 0 || curY > src.Height - 1 || curX > src.Width - 1)
+                                    continue;
+
+                                var iSx = gx.At<double>(curY, curX, 0);
+                                var iSy = gy.At<double>(curY, curX, 0);
+
+                                if ((iSx != 0 || iSy != 0) && (iTx != 0 || iTy != 0))
+                                {
+                                    var mag = magnitude.At<double>(curY, curX, 0);
+                                    var matGradMag = mag == 0 ? 0 : 1 / mag; // 1/√(dx²+dy²)
+                                    partialSum += ((iSx * iTx) + (iSy * iTy)) * (item.Magnitude * matGradMag);
+                                }
+
+                                var sumOfCoords = m + 1;
+                                partialScore = partialSum / sumOfCoords;
+                                /// check termination criteria
+                                /// if partial score score is less than the score than needed to make the required score at that position
+                                /// break serching at that coordinate.
+                                if (partialScore < Math.Min((minScore - 1) + normGreediness * sumOfCoords, normMinScore * sumOfCoords))
+                                    break;
+                            }
+                            if (partialScore > resultScore)
+                            {
+                                resultScore = partialScore;
+                                center.X = j;
+                                center.Y = i;
+                            }
+                        }
+                    }
+
+                    /// overlay display origin image, edge(green) and center point(red)
+                    Cv2.DrawContours(destination, new[] { results.Select(_ => _.Offset.ToPoint()) }, -1, Scalar.LightGreen, 2, offset: center);
+                    Cv2.Circle(destination, center, 5, Scalar.Red, -1);
+                    Trace.TraceInformation($"NCC matching score {resultScore}. time: {stopwatch.Elapsed.TotalMilliseconds} ms");
+                }
+
+                RaisePropertyChanged(nameof(Destination));
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                Trace.TraceError(ex.StackTrace);
+            }
+            finally
+            {
+                stopwatch.Stop();
+            }
+        }
+    }
+
+    public class PointInfo
+    {
+        /// <summary>
+        /// Point of edge 
+        /// </summary>
+        /// <remarks>
+        /// (x,y)
+        /// </remarks>
+        public Point Point;
+
+        /// <summary>
+        /// Center of edge 
+        /// </summary>
+        /// <remarks>
+        /// (x0,y0)
+        /// </remarks>
+        public Point2d Center { get; private set; }
+
+        /// <summary>
+        /// Point-Center 
+        /// </summary>
+        /// <remarks>
+        /// (x-x0,y-y0)
+        /// </remarks>
+        public Point2d Offset { get; private set; }
+
+        /// <summary>
+        /// Derivative at Point
+        /// </summary>
+        /// <remarks>
+        /// (dx,dy)
+        /// </remarks>
+        public Point2d Derivative;
+
+        /// <summary>
+        /// Magnitude at Point
+        /// </summary>
+        /// <remarks>
+        /// 1/√(dx²+dy²)
+        /// </remarks>
+        public double Magnitude;
+
+        /// <summary>
+        /// Direction at Point
+        /// </summary>
+        /// <remarks>
+        /// atan2(dy,dx) (not currently in use)
+        /// </remarks>
+        public double Direction;
+
+        /// <summary>
+        /// Calc Offset with Point by center
+        /// </summary>
+        /// <param name="center"></param>
+        public void Update(Point2d center)
+        {
+            Center = center;
+            Offset = Point - center;
+        }
+    }
+}
